2bf62148ef1ae99e939bd1359fc29717c1a8a0f8
Justin Arista Kasin <30282499+zegheim@users.noreply.github.com>
2022-11-11 19:03:02 +0000

    make resource names more generic

--- lambda.tf
+++ lambda.tf
@@ -10,7 +10,7 @@ data "aws_iam_policy_document" "lambda_assume_role_policy_document" {
 }
 
 resource "aws_iam_role" "lambda_exec_role" {
-  name               = "${var.project_name}-lambda-execution-role"
+  name               = "${var.lambda_name}-execution-role"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy_document.json
 }
 
